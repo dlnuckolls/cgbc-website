ca3389c3d44bd0eaf6fb9c8ac3b544d49afbe0f9
David Nuckolls <dlnuckolls@gmail.com>
2020-01-18 13:25:25 -0500

    Add base helper classes

--- Cedar Grove/Cedar Grove/Default.aspx.cs
+++ Cedar Grove/Cedar Grove/Default.aspx.cs
@@ -6,7 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace Cedar_Grove {
-  public partial class Default :System.Web.UI.Page {
+  public partial class Default :BasePage {
     protected void Page_Load(object sender, EventArgs e) {
 
     }
---
+++ Cedar Grove/Cedar Grove/helpers/SessionManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cedar_Grove {
+  public class SessionManager {
+    public string UserId;
+    public bool IsAuthenticated;
+    public bool IsAdmin;
+
+  }
+}
