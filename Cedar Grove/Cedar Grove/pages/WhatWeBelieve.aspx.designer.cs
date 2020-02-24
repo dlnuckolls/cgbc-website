293bcd4be26591674cc0cbd946c7047f7ce13f59
David Nuckolls <dlnuckolls@gmail.com>
2020-02-23 22:30:40 -0500

    Fix styling on Believe page

--- Cedar Grove/Cedar Grove/pages/WhatWeBelieve.aspx.designer.cs
+++ Cedar Grove/Cedar Grove/pages/WhatWeBelieve.aspx.designer.cs
@@ -20,5 +20,23 @@ namespace Cedar_Grove.pages {
     /// To modify move field declaration from designer file to code-behind file.
     /// </remarks>
     protected global::System.Web.UI.WebControls.Literal TitleTag;
+
+    /// <summary>
+    /// JumbotronLayout control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::Telerik.Web.UI.RadPageLayout JumbotronLayout;
+
+    /// <summary>
+    /// PageContentBlock control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.Literal PageContentBlock;
   }
 }
